59640a354efd8657342c60e2303630f0d5b9f620
Easton Scowden <es686918@ohio.edu>
2019-11-05 22:42:29 -0500

    added the functions printLn and addOpt

--- car-dealer.cc
+++ car-dealer.cc
@@ -10,36 +10,87 @@
 #include <iomanip>
 #include <cstdlib>
 #include <cctype>
+#include <vector> 
+#include <fstream>
+#include <string>
+#include <algorithm> 
 using namespace std;
 
 ///function prototypes
 
 void displayMenu(int &choice) ; // a function that displays the option menu
 void optionOne(char model, double &total, string &modelOfCar, int &choice) ; // a function that picks the model based on user input
+void addOpt(string &option, vector<string> names, vector<double> prices, vector<string> &selectedChoice, double &total, int &choice) ; 
+void printLn(vector<string> selectedChoice, double total, char model) ; 
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) { 
+    vector<string> names(30) ; 
+    vector<string> selectedChoice(0) ; 
+    vector<double> prices(30) ;
+    ifstream in ; 
     int choice ; 
-    char model ;
-    double total = 0; 
+    char model = 'P' ;
+    double total = 0 ; 
     string modelS ; 
+    int count = 0 ;
+    int vCount = 0 ;  
+    string option ; 
+    double ePrice = 10000 ; 
+    double lPrice = 12000 ; 
+    double xPrice = 18000 ; 
 
-    cout.setf(ios::fixed);
-    cout.setf(ios::showpoint);
-    cout.precision(2);
+    cout.setf(ios::fixed); 
+    cout.setf(ios::showpoint); 
+    cout.precision(2); 
+    
+    in.open("options.txt") ; 
+
+    if(in.fail()) {
+        cout << "File error" << endl ; 
+        return -1 ; 
+    }
+
+    while(in >> prices[vCount]){
+        getline(in, names[vCount]) ;
+        vCount ++ ;  
+    }
+
+    for(int i = 0; i < vCount; i++){
+        names[i] = names[i].substr(1, (names[i]).length()-1) ; 
+    }
+
+    in.close() ; 
 
     displayMenu(choice) ; 
 
     while(choice != 6){
         switch(choice){
             case 1:
-                cout << "Enter the model (E, L, X): " ;
-                cin >> model ; 
-                optionOne(model, total, modelS, choice) ; 
+                count ++ ; 
+                if(count > 1){
+                    if(model == 'E'){
+                        total -= ePrice ; 
+                    } else if(model == 'L'){
+                        total -= lPrice ; 
+                    } else{
+                        total -= xPrice  ;
+                    }
+                    cout << "Enter the model (E, L, X): " ;
+                    cin >> model ; 
+                    optionOne(model, total, modelS, choice) ; 
+                } else {
+                    cout << "Enter the model (E, L, X): " ;
+                    cin >> model ; 
+                    optionOne(model, total, modelS, choice) ; 
+                }
+                printLn(selectedChoice, total, model) ; 
                 break ;
             case 2:
 
             case 3:
-
+                addOpt(option, names, prices, selectedChoice, total, choice) ;
+                printLn(selectedChoice, total, model) ; 
+                break ; 
             case 4:
             
             case 5:
@@ -51,8 +102,6 @@ int main(int argc, char const *argv[]) {
         }
         displayMenu(choice) ; 
     }
-        
-
     return 0;
 }/// main
 
@@ -71,14 +120,53 @@ void optionOne(char model, double &total, string &modelOfCar, int &choice){
     model = toupper(model) ; 
     if(model == 'E'){
         modelOfCar = "Model: E, " ; 
-        total = total + 10000 ;  
+        total += 10000 ;  
     } else if(model == 'L'){
         modelOfCar = "Model: L, " ; 
-        total = total + 12000 ; 
+        total += 12000 ; 
     } else if(model == 'X'){
         modelOfCar = "Model: X, " ; 
-        total = total + 18000 ; 
+        total += 18000 ; 
     } else{
        ;
+    }
+}
+
+void addOpt(string &option, vector<string> names, vector<double> prices, vector<string> &selectedChoice, double &total, int &choice){
+    int count = 0 ;
+    cout << "Enter an option name: " ; 
+    cin >> ws ;
+    getline(cin, option) ; 
+
+    if(find(names.begin(), names.end(), option) != names.end()){
+        if(find(selectedChoice.begin(), selectedChoice.end(), option) != selectedChoice.end()){
+            cout << "You have already selected this." << endl ; 
+        }  else {
+            selectedChoice.push_back(option) ; 
+        }
+    } else {
+        cout << "Invalid option." << endl ; 
+    }
+
+    while(names[count] != option){
+        count ++ ; 
+    }
+    
+    total += prices[count] ; 
+}
+
+void printLn(vector<string> selectedChoice, double total, char model){
+    if(model == 'P'){
+        cout << "Please enter a model." << endl ; 
+    } else {
+        cout << "Model " << model << " " << total << ", " << "Options: " ;
+        if(selectedChoice.size() == 0){
+            cout << endl ; 
+        } else {
+            for(size_t i = 0; i < selectedChoice.size(); i++){
+            cout << selectedChoice[i] << ", " ; 
+            }
+            cout << endl ; 
+        }
     }
 }
